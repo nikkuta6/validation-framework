47b3e2827fe67c4c22fd33ac2cc3f5dc9a3e942f
NikitaPranovich <nikitapranovi@gmail.com>
2026-04-30 21:42:00 +0300

    update pom.xml

--- src/main/java/by/pranovich/validationframework/factory/ValidationHandlerChainFactory.java
+++ src/main/java/by/pranovich/validationframework/factory/ValidationHandlerChainFactory.java
@@ -12,29 +12,29 @@ import by.pranovich.validationframework.handler.ValidationHandler;
 
 public class ValidationHandlerChainFactory {
 
-  private ValidationHandlerChainFactory() {
-  }
+    private ValidationHandlerChainFactory() {
+    }
 
-  public static ValidationHandler createDefaultChain() {
-    ValidationHandler notNullHandler = new NotNullHandler();
-    ValidationHandler patternHandler = new PatternHandler();
-    ValidationHandler emailHandler = new EmailHandler();
-    ValidationHandler sizeHandler = new SizeHandler();
-    ValidationHandler maxHandler = new MaxHandler();
-    ValidationHandler minHandler = new MinHandler();
-    ValidationHandler positivHandler = new PositiveHandler();
-    ValidationHandler negativHandler = new NegativeHandler();
+    public static ValidationHandler createDefaultChain() {
+        ValidationHandler notNullHandler = new NotNullHandler();
+        ValidationHandler patternHandler = new PatternHandler();
+        ValidationHandler emailHandler = new EmailHandler();
+        ValidationHandler sizeHandler = new SizeHandler();
+        ValidationHandler maxHandler = new MaxHandler();
+        ValidationHandler minHandler = new MinHandler();
+        ValidationHandler positiveHandler = new PositiveHandler();
+        ValidationHandler negativeHandler = new NegativeHandler();
 
-    notNullHandler
-        .linkWith(patternHandler)
-        .linkWith(emailHandler)
-        .linkWith(sizeHandler)
-        .linkWith(maxHandler)
-        .linkWith(minHandler)
-        .linkWith(positivHandler)
-        .linkWith(negativHandler);
+        notNullHandler
+                .linkWith(patternHandler)
+                .linkWith(emailHandler)
+                .linkWith(sizeHandler)
+                .linkWith(maxHandler)
+                .linkWith(minHandler)
+                .linkWith(positiveHandler)
+                .linkWith(negativeHandler);
 
-    return notNullHandler;
-  }
+        return notNullHandler;
+    }
 
 }
